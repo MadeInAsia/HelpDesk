37607b6a3b19ab179689b555c38b40a3de673903
Justin Brown <jvwbrown0@gmail.com>
2025-02-24 20:51:00 +0100

    3 hours of bug fixes later fmllllll finally everything should work sweet and sound

--- HelpDesk/WinUser.Designer.cs
+++ HelpDesk/WinUser.Designer.cs
@@ -29,8 +29,6 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.bunifuCircleProgress1 = new Bunifu.UI.WinForms.BunifuCircleProgress();
-            this.bunifuRadialGauge1 = new Bunifu.UI.WinForms.BunifuRadialGauge();
             this.SuspendLayout();
             // 
             // label1
@@ -42,86 +40,11 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "user";
             // 
-            // bunifuCircleProgress1
-            // 
-            this.bunifuCircleProgress1.Animated = false;
-            this.bunifuCircleProgress1.AnimationInterval = 1;
-            this.bunifuCircleProgress1.AnimationSpeed = 1;
-            this.bunifuCircleProgress1.BackColor = System.Drawing.Color.Transparent;
-            this.bunifuCircleProgress1.CircleMargin = 10;
-            this.bunifuCircleProgress1.Font = new System.Drawing.Font("Microsoft Sans Serif", 40F, System.Drawing.FontStyle.Bold);
-            this.bunifuCircleProgress1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.bunifuCircleProgress1.IsPercentage = false;
-            this.bunifuCircleProgress1.LineProgressThickness = 10;
-            this.bunifuCircleProgress1.LineThickness = 10;
-            this.bunifuCircleProgress1.Location = new System.Drawing.Point(553, 230);
-            this.bunifuCircleProgress1.Name = "bunifuCircleProgress1";
-            this.bunifuCircleProgress1.ProgressAnimationSpeed = 200;
-            this.bunifuCircleProgress1.ProgressBackColor = System.Drawing.Color.Gainsboro;
-            this.bunifuCircleProgress1.ProgressColor = System.Drawing.Color.DodgerBlue;
-            this.bunifuCircleProgress1.ProgressColor2 = System.Drawing.Color.DodgerBlue;
-            this.bunifuCircleProgress1.ProgressEndCap = Bunifu.UI.WinForms.BunifuCircleProgress.CapStyles.Round;
-            this.bunifuCircleProgress1.ProgressFillStyle = Bunifu.UI.WinForms.BunifuCircleProgress.FillStyles.Solid;
-            this.bunifuCircleProgress1.ProgressStartCap = Bunifu.UI.WinForms.BunifuCircleProgress.CapStyles.Round;
-            this.bunifuCircleProgress1.SecondaryFont = new System.Drawing.Font("Microsoft Sans Serif", 16F);
-            this.bunifuCircleProgress1.Size = new System.Drawing.Size(184, 184);
-            this.bunifuCircleProgress1.SubScriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
-            this.bunifuCircleProgress1.SubScriptMargin = new System.Windows.Forms.Padding(5, -20, 0, 0);
-            this.bunifuCircleProgress1.SubScriptText = ".00";
-            this.bunifuCircleProgress1.SuperScriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(166)))), ((int)(((byte)(166)))));
-            this.bunifuCircleProgress1.SuperScriptMargin = new System.Windows.Forms.Padding(5, 20, 0, 0);
-            this.bunifuCircleProgress1.SuperScriptText = "°C";
-            this.bunifuCircleProgress1.TabIndex = 16;
-            this.bunifuCircleProgress1.Text = "30";
-            this.bunifuCircleProgress1.TextMargin = new System.Windows.Forms.Padding(0, 5, 0, 0);
-            this.bunifuCircleProgress1.Value = 30;
-            this.bunifuCircleProgress1.ValueByTransition = 30;
-            this.bunifuCircleProgress1.ValueMargin = new System.Windows.Forms.Padding(0, 5, 0, 0);
-            // 
-            // bunifuRadialGauge1
-            // 
-            this.bunifuRadialGauge1.AutoGenerateProgressColorWhenHigh = true;
-            this.bunifuRadialGauge1.AutoGenerateProgressColorWhenLow = false;
-            this.bunifuRadialGauge1.BackColor = System.Drawing.Color.Transparent;
-            this.bunifuRadialGauge1.Font = new System.Drawing.Font("Century Gothic", 18F);
-            this.bunifuRadialGauge1.LighteningFactor = 70;
-            this.bunifuRadialGauge1.Location = new System.Drawing.Point(553, 78);
-            this.bunifuRadialGauge1.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
-            this.bunifuRadialGauge1.Maximum = 100;
-            this.bunifuRadialGauge1.Minimum = 0;
-            this.bunifuRadialGauge1.Name = "bunifuRadialGauge1";
-            this.bunifuRadialGauge1.Prefix = "";
-            this.bunifuRadialGauge1.ProgressBackColor = System.Drawing.Color.LightBlue;
-            this.bunifuRadialGauge1.ProgressBgColor = System.Drawing.Color.LightBlue;
-            this.bunifuRadialGauge1.ProgressCap = Bunifu.UI.WinForms.BunifuRadialGauge.CapStyles.Flat;
-            this.bunifuRadialGauge1.ProgressColor = System.Drawing.Color.DodgerBlue;
-            this.bunifuRadialGauge1.ProgressColor1 = System.Drawing.Color.DodgerBlue;
-            this.bunifuRadialGauge1.ProgressColor2 = System.Drawing.Color.Crimson;
-            this.bunifuRadialGauge1.ProgressColorHigh = System.Drawing.Color.Crimson;
-            this.bunifuRadialGauge1.ProgressColorLow = System.Drawing.Color.DodgerBlue;
-            this.bunifuRadialGauge1.ProgressHighValueMark = 70;
-            this.bunifuRadialGauge1.RangeEnd = 100;
-            this.bunifuRadialGauge1.RangeLabelsColor = System.Drawing.Color.Black;
-            this.bunifuRadialGauge1.RangeLabelsFont = new System.Drawing.Font("Segoe UI", 8F);
-            this.bunifuRadialGauge1.RangeStart = 0;
-            this.bunifuRadialGauge1.ShowRangeLabels = true;
-            this.bunifuRadialGauge1.ShowValueLabel = true;
-            this.bunifuRadialGauge1.Size = new System.Drawing.Size(189, 126);
-            this.bunifuRadialGauge1.Suffix = "%";
-            this.bunifuRadialGauge1.TabIndex = 17;
-            this.bunifuRadialGauge1.Thickness = 30;
-            this.bunifuRadialGauge1.Value = 40;
-            this.bunifuRadialGauge1.ValueByTransition = 40;
-            this.bunifuRadialGauge1.ValueLabelColor = System.Drawing.Color.Black;
-            this.bunifuRadialGauge1.WarningMark = 70;
-            // 
             // WinUser
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.bunifuRadialGauge1);
-            this.Controls.Add(this.bunifuCircleProgress1);
             this.Controls.Add(this.label1);
             this.Name = "WinUser";
             this.Text = "user";
@@ -133,7 +56,5 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private Bunifu.UI.WinForms.BunifuCircleProgress bunifuCircleProgress1;
-        private BunifuRadialGauge bunifuRadialGauge1;
     }
 }
